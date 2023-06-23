5d2e6ba1d316a878a2589ea94b8ac9cc91406869
Bich Vu <vtbich1011@hotmail.com>
2023-06-23 18:17:10 +0700

    update route for angular app

--- TodoApp.WebApp/Client/src/modules/app.routes.ts
+++ TodoApp.WebApp/Client/src/modules/app.routes.ts
@@ -5,8 +5,7 @@ import { HomeComponent } from './home/home-component';
 
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent }
+  { path: '', component: HomeComponent }
 ];
 
 export const routing = RouterModule.forRoot(routes);
