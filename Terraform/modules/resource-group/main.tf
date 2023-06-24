e3f00a99ea1c47fd948a6cd1312f1e3b5d555dd2
Bich Vu <vtbich1011@hotmail.com>
2023-06-24 20:43:14 +0700

    fix bug

--- Terraform/modules/resource-group/main.tf
+++ Terraform/modules/resource-group/main.tf
@@ -8,8 +8,8 @@ resource "azurerm_resource_group" "demoapp_resource_group" {
   }
 }
 
-resource "azurerm_role_assignment" "demoapp_api_rg_role_assign_to_azure_devops" {
-  role_definition_name = "Contributor"
-  scope = azurerm_resource_group.demoapp_resource_group.id
-  principal_id = var.azure_devops_project_id
-}
+# resource "azurerm_role_assignment" "demoapp_api_rg_role_assign_to_azure_devops" {
+#   role_definition_name = "Contributor"
+#   scope = azurerm_resource_group.demoapp_resource_group.id
+#   principal_id = var.azure_devops_project_id
+# }
