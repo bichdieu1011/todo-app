05ea4efc984a004cda2157a52c9251fdc309f9f2
Bich Vu <vtbich1011@hotmail.com>
2023-06-25 09:43:12 +0700

    revert role assigmnet for resource group

--- Terraform/modules/resource-group/main.tf
+++ Terraform/modules/resource-group/main.tf
@@ -2,6 +2,11 @@
 data "azurerm_client_config" "current" {
 }
 
+data "azurerm_builtin_role_definition" "Role" {
+  name = "Contributor"
+}
+
+
 resource "azurerm_resource_group" "demoapp_resource_group" {
   name     = var.resource_group
   location = var.deploy_location
@@ -10,10 +15,4 @@ resource "azurerm_resource_group" "demoapp_resource_group" {
     department = var.department
     source = var.app_source
   }
-}
-
-resource "azurerm_role_assignment" "demoapp_api_rg_role_assign_to_azure_devops" {
-  role_definition_name = "Contributor"
-  scope = azurerm_resource_group.demoapp_resource_group.id
-  principal_id = data.azurerm_client_config.current.object_id
 }
