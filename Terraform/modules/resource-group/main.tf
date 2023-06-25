f3ff597e8433f97777be2fb4a8a6384234ee5f06
Bich Vu <vtbich1011@hotmail.com>
2023-06-25 09:44:59 +0700

    revert change in resource-group/main.tf

--- Terraform/modules/resource-group/main.tf
+++ Terraform/modules/resource-group/main.tf
@@ -2,11 +2,6 @@
 data "azurerm_client_config" "current" {
 }
 
-data "azurerm_builtin_role_definition" "Role" {
-  name = "Contributor"
-}
-
-
 resource "azurerm_resource_group" "demoapp_resource_group" {
   name     = var.resource_group
   location = var.deploy_location
