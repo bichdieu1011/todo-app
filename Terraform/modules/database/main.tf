9ef2c37d9de17d3a311fe9074249e9ad9bec6896
Bich Vu <vtbich1011@hotmail.com>
2023-06-27 11:49:13 +0700

    add firewall rule to access sql server from azure services

--- Terraform/modules/database/main.tf
+++ Terraform/modules/database/main.tf
@@ -1,12 +1,12 @@
 
 resource "azurerm_mssql_server" "demoapp_sql_server" {
-  name                         = "sql-us-demoapp-sqlserver"
-  resource_group_name          = var.resource_group
-  location                     = var.deploy_location
-  version                      = "12.0"
-  administrator_login          = var.admin_username
-  administrator_login_password = var.admin_password
-
+  name                          = "sql-us-demoapp-sqlserver"
+  resource_group_name           = var.resource_group
+  location                      = var.deploy_location
+  version                       = "12.0"
+  administrator_login           = var.admin_username
+  administrator_login_password  = var.admin_password
+  public_network_access_enabled = true
   tags = {
     environment = var.environment
     department  = var.department
@@ -28,4 +28,11 @@ resource "azurerm_mssql_database" "demoapp_sql_database" {
     department  = var.department
     source      = var.app_source
   }
+}
+
+resource "azurerm_mssql_firewall_rule" "demoapp_sql_server" {
+  name             = "FirewallRuleForAzureServices"
+  server_id        = azurerm_mssql_server.demoapp_sql_server.id  
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
 }
