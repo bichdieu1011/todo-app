55ab6914fb8ef80bcaf45040dcda5011b868f20b
Vũ Thị Bích <bichdieu1011@gmail.com>
2023-06-30 11:05:24 +0700

    Update environment.ts

--- TodoApp.WebApp/Client/src/environment/environment.ts
+++ TodoApp.WebApp/Client/src/environment/environment.ts
@@ -4,10 +4,5 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  TD_BASE_URL: "https://localhost:7142/",
-  TD_APP_ID: "520be1a4-0b4f-4287-9ccb-0f230aabf190",
-  TD_CLIENT_ID: "e77c3e09-5973-47c9-9c5d-6e612df9c4a1",
-  TD_REDIRECT_URL: "http://locahost:4200"
-
+  production: false
 };
